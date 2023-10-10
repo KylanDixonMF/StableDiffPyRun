d4e3dc8836e7234dff68c485ed1e1092015ecae7
AutonomyHomeBase <kylandixon23@gmail.com>
2023-10-10 16:45:56 -0700

    New error

--- Base.py
+++ Base.py
@@ -4,7 +4,7 @@ import os
 
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
-pipe.pipe.enable_model_cpu_offload()
+pipe.to("cuda")
 
 # if using torch < 2.0
 # pipe.enable_xformers_memory_efficient_attention()
